97b1c6f5da0606693a12eada599e0869bdbcaa0d
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-12-07 20:37:51 -0800

    feat: use server cookies and server component to handle auth WIP

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,34 +1,39 @@
-"use client";
-import { useEffect, useState } from "react";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const getTasks = async () => {
   const url = `http://localhost:8080/tasks`;
-
+  const request = new Request(url);
+  const cookieStore = cookies();
+  const cookie = cookieStore.get("time_bandit_auth_token_v1");
   try {
-    let res = await fetch(url, {
+    let res = await fetch(request, {
       method: "GET",
       mode: "cors",
       credentials: "include",
       headers: {
         "Access-Control-Allow-Credentials": "true",
+        Cookie: `time_bandit_auth_token_v1=${cookie?.value || ""}`,
       },
     });
-    return await res.json();
+    if (res.ok) {
+      return res.json();
+    } else {
+      const error = new Error("UNAUTHORIZED");
+      throw error;
+    }
   } catch (e) {
-    console.log(`${e}`);
+    JSON.stringify(e);
+    console.error(e);
   }
 };
 
-const Dashboard = () => {
-  const [tasks, setTasks] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      const userTasks = await getTasks();
-      setTasks(userTasks);
-    };
-    fetchData();
-  }, []);
-
+const Dashboard = async () => {
+  const tasks = await getTasks();
+  if (!tasks) {
+    redirect("/");
+  }
+  console.log({ tasks });
   return <div>{JSON.stringify(tasks)}</div>;
 };
 
